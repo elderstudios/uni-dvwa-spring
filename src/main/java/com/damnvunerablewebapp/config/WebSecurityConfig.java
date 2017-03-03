ad2fd1e20fa17db8d338eceb6db837d4c9e4943f
Rick Whalley <rickwhalley@Ricks-MacBook-Pro.local>
2017-03-03 09:10:33 +0000

    - Quick noob mode CSS added to all templates. - Enabled Secure mode for annotations

--- src/main/java/com/damnvunerablewebapp/config/WebSecurityConfig.java
+++ src/main/java/com/damnvunerablewebapp/config/WebSecurityConfig.java
@@ -3,12 +3,14 @@ package com.damnvunerablewebapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
