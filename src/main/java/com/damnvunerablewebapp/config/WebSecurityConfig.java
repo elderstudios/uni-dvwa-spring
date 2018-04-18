36773986e97cede475b1784ebff0cd6ea7456fcb
sandi Ardyansyah <sandyQx@gmail.com>
2018-04-19 04:06:00 +0700

    update style

--- src/main/java/com/damnvunerablewebapp/config/WebSecurityConfig.java
+++ src/main/java/com/damnvunerablewebapp/config/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
--- src/main/java/com/damnvunerablewebapp/domain/MigrationService.java
+++ src/main/java/com/damnvunerablewebapp/domain/MigrationService.java
@@ -21,10 +21,10 @@ public class MigrationService {
 
     @PostConstruct
     private void init(){
-        userInfoRepository.save(new UserInfo().setId(3).setEmail("user1@numisec.com").setUsername("user1").setBankBalance("£1001.00").setAdminNote("Really bad coder. Don't trust with security."));
-        userInfoRepository.save(new UserInfo().setId(4).setEmail("user2@numisec.com").setUsername("user2").setBankBalance("£1002.00").setAdminNote("Really bad coder2."));
-        userInfoRepository.save(new UserInfo().setId(5).setEmail("user3@numisec.com").setUsername("user3").setBankBalance("£1003.00").setAdminNote("Really bad coder3."));
-        userInfoRepository.save(new UserInfo().setId(1).setEmail("admin1@numisec.com").setUsername("admin1").setBankBalance("£9001.00").setAdminNote("I'm the best1."));
-        userInfoRepository.save(new UserInfo().setId(2).setEmail("admin2@numisec.com").setUsername("admin2").setBankBalance("£9002.00").setAdminNote("I'm the best2."));
+        userInfoRepository.save(new UserInfo().setId(3).setLevel("User").setName("Carmun").setEmail("carmun@users.com").setUsername("user1").setBankBalance("£1001.00").setAdminNote("Really bad coder. Don't trust with security."));
+        userInfoRepository.save(new UserInfo().setId(4).setLevel("User").setName("David").setEmail("david@users.com").setUsername("user2").setBankBalance("£1002.00").setAdminNote("Really bad coder 2."));
+        userInfoRepository.save(new UserInfo().setId(5).setLevel("User").setName("Nicole").setEmail("nicole@users.com").setUsername("user3").setBankBalance("£1003.00").setAdminNote("Really bad coder 3."));
+        userInfoRepository.save(new UserInfo().setId(1).setLevel("Administrator").setName("Jhon").setEmail("jhon@myadmin.com").setUsername("admin1").setBankBalance("£9001.00").setAdminNote("I'm the best 1."));
+        userInfoRepository.save(new UserInfo().setId(2).setLevel("Administrator").setName("Evelin").setEmail("evelin@myadmin.com").setUsername("admin2").setBankBalance("£9002.00").setAdminNote("I'm the best 2."));
     }
 }
