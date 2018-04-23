fa188d2b4638f6ca78ee6009a1be7d09f7228a77
if08017@gmail.com <if08017@gmail.com>
2018-04-24 03:13:04 +0700

    Chore[update content] clean code

--- build.groovy
+++ build.groovy
@@ -14,7 +14,7 @@ node {
 		sh ("sudo docker images")
 	}
 	stage('Spotbugs+FinSecBugs'){
-		sh ('echo Spotbugs')
+		sh ("echo Spotbugs")
 	}
 	stage('Dependency-check'){
 		sh ("echo Spotbugs")
@@ -25,5 +25,3 @@ node {
 	stage('Postman Nemwan'){
 		sh ("echo Spotbugs")
 	}
-
-	sh 'docker run --rm -t -v `pwd`:/dvwa-spring docker.io/devsecopsat/spotbugs -textui -low -nested:false -quiet -exclude /dvwa-spring/spotbugs-customExcludeFilter.xml -pluginList /findsecbugs-plugin-1.7.1.jar -output /dvwa-spring/spotbugs-dvwa-spring-result.html -html:fancy-hist.xsl  /dvwa-spring/target/www-0.0.1-SNAPSHOT.jar'
